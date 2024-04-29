aa3a0601f2c8911af80113f5e56e93d844ebc6f2
mobileexpert1 <appsdev096@gmail.com>
2024-04-29 18:07:34 +0530

    update new filee

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
 			path: "Sources",
 			resources: [
 				.process("Resource/Media.xcassets"),
-                .process("ResourceFile/iPass.license"),
+                .process("Classes/iPass.license"),
                 .process("ResourceFile/Certificates.bundle"),
                 .process("ResourceFile/CertificatesPA.bundle"),
                 .process("ResourceFile/CertificatesTA.bundle")
