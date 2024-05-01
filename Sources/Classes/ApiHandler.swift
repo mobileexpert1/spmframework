cb2ef4de1f1073362d1b3a3da38beb47e545b731
mobileexpert1 <appsdev096@gmail.com>
2024-05-01 16:10:10 +0530

    updated aws view

--- Sources/Classes/ApiHandler.swift
+++ Sources/Classes/ApiHandler.swift
@@ -247,7 +247,7 @@ public class iPassHandler {
     
     public static func getresultliveness(token: String, sessionId: String, sid: String, email: String, auth_token: String, completion: @escaping (Data?, Error?) -> Void) {
             
-        if var urlComponents = URLComponents(string: "https://plusapi.ipass-mena.com/api/v1/ipass/plus/session/result") {
+        if var urlComponents = URLComponents(string: "https://plusapi.ipass-mena.com/api/v1/ipass/plus/session/result/") {
             urlComponents.queryItems = [ //
                 
                 URLQueryItem(name: "sessionId", value: sessionId),
