1b36f4777eff3a60953a23c66898974b9c20ea6c
mobileexpert1 <appsdev096@gmail.com>
2024-06-03 13:08:08 +0530

    data

--- Package.swift
+++ Package.swift
@@ -32,6 +32,9 @@ let package = Package(
         ],
 
 			path: "Sources",
+            exclude: [
+                                     "PrivateResources" 
+                       ],
 			resources: [
                 .process("Media.xcassets"),
                 .process("Certificates.bundle"),
